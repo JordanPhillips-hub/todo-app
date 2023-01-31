3856414b70d7cf56cc25b89ca094ae69f2dd5809
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-01-31 10:21:48 -0600

    Delete header component
    Moved header to app component finished setting up light and dark theme switch

---
+++ src/components/App/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import useLocalStorage from "use-local-storage";
+import Moon from "../../assets/images/icon-moon.svg";
+import Sun from "../../assets/images/icon-sun.svg";
+import "./App.scss";
+
+function App() {
+  const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
+
+  const toggleTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme);
+  };
+
+  return (
+    <div className="App" data-theme={theme}>
+      <header>
+        <h1>TODO</h1>
+        <img
+          onClick={toggleTheme}
+          src={theme === "light" ? Moon : Sun}
+          alt="Toggle Theme"
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
