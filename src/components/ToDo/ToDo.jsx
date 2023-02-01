fd790d31cecd77ad98314718ef383afea4f9a7c2
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-02-01 11:37:01 -0600

    Add state for todo input
    Added state to hold input text, created method to set the state of input test on change

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -4,12 +4,21 @@ import "../ToDo/ToDo.scss";
 class ToDo extends React.Component {
   constructor() {
     super();
+    this.state = {
+      text: "",
+    };
   }
+
+  handleChange = ({ target: { value } }) =>
+    this.setState({
+      text: value,
+    });
 
   render() {
     return (
       <form className="todo">
         <input
+          onChange={this.handleChange}
           className="todo__input"
           type="text"
           placeholder="Create a new todo..."
