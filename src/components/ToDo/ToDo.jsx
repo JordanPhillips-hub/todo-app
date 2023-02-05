827e43982df7d07bccd45ec8a64729b4a13ee39a
JordanPhillips-hub <jordan.phillips.988@gmail.com>
2023-02-04 21:11:32 -0600

    Add items state and submit form functionality

--- src/components/ToDo/ToDo.jsx
+++ src/components/ToDo/ToDo.jsx
@@ -8,6 +8,7 @@ class ToDo extends React.Component {
     super();
     this.state = {
       text: "",
+      items: [],
     };
   }
 
@@ -18,138 +19,67 @@ class ToDo extends React.Component {
 
   handleCompleteItem = (e) => {
     const { target: btn } = e;
-
     const checkMark = btn.nextSibling;
+    const listItem = checkMark.parentElement.nextSibling;
     btn.classList.toggle("showBackground");
     checkMark.classList.toggle("showCheckMark");
-
-    const listItem = checkMark.parentElement.nextSibling;
     listItem.classList.toggle("complete");
+  };
+
+  handleSubmit = (e) => {
     e.preventDefault();
+    const { text } = this.state;
+    const newItem = {
+      text: text,
+    };
+
+    this.setState((prev) => ({
+      items: [...prev.items, newItem],
+      text: "",
+    }));
   };
 
   render() {
+    const { items } = this.state;
+
     return (
-      <form className="todo">
+      <form onSubmit={this.handleSubmit} className="todo">
         <input
           onChange={this.handleTodoInput}
           className="todo__input"
           type="text"
           placeholder="Create a new todo..."
+          autoComplete="off"
         />
 
         <ul className="todoList">
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
+          {items.map((item, index) => {
+            return (
+              <div className="listItem">
+                <div className="completeItem">
+                  <button
+                    onClick={this.handleCompleteItem}
+                    className="btn completeItem--btn"
+                  />
+                  <img
+                    className="completeItem__checkMark"
+                    src={checkMark}
+                    alt="checked"
+                  />
+                </div>
 
-            <li className="listItem__item">
-              Complete online JavaScript course
-            </li>
+                <li key={index} className="listItem__item">
+                  {item.text}
+                </li>
 
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
-
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
-
-            <li className="listItem__item">Jog around the park 3x</li>
-
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
-
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
-
-            <li className="listItem__item">10 minutes meditation</li>
-
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
-
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
-
-            <li className="listItem__item">Read for 1 hour</li>
-
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
-
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
-
-            <li className="listItem__item">Pick up groceries</li>
-
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
-
-          <div className="listItem">
-            <div className="completeItem">
-              <button
-                onClick={this.handleCompleteItem}
-                className="btn completeItem--btn"
-              />
-              <img
-                className="completeItem__checkMark"
-                src={checkMark}
-                alt="checked"
-              />
-            </div>
-
-            <li className="listItem__item">
-              Complete Todo App on Frontend Mentor
-            </li>
-
-            <img className="listItem__delete" src={cross} alt="Delete Item" />
-          </div>
+                <img
+                  className="listItem__delete"
+                  src={cross}
+                  alt="Delete Item"
+                />
+              </div>
+            );
+          })}
         </ul>
       </form>
     );
